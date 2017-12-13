5d471954cb417dad1caf538cca39a01466657bbe
jannekenal <32904213+jannekenal@users.noreply.github.com>
2017-12-13 10:33:50 +0100

    Add files via upload
    Login system ive made with help

---
+++ Index.php
@@ -0,0 +1,36 @@
+<?php
+	include_once 'header.php';
+?>
+<section class="main-container">
+	<div class="main-wrapper">
+		<h2>Home</h2>
+
+		
+<?php
+
+	if (isset($_SESSION['u_id'])) {
+		echo '<script language = "javascript">';
+		echo 'alert("You are logged in")';
+		echo '</script>';
+		exit();
+	}
+
+
+
+
+
+
+?>
+
+
+	
+	</div>
+</section>
+
+
+<?php
+
+include_once 'footer.php';
+
+?>
+
